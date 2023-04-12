d9167168f6326b3792e102d49116744364e70c6c
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2023-04-12 12:40:41 +0100

    update favicon generation

--- bin/make_favicons.py
+++ bin/make_favicons.py
@@ -1,13 +1,7 @@
 from favicons import Favicons
-from pathlib import Path
 
-favicon_dir = "assets/favicons/"
-base_favicon = favicon_dir+"rsg/rsg_fav_base.png"
-generated_dir = favicon_dir+"temp/"
+favicon_dir = "assets/favicons/rsg/"
+base_favicon = favicon_dir + "rsg_fav_base.png"
 
-p = Path(generated_dir)
-p.mkdir(parents=True, exist_ok=True)
-
-#genterates favicons into a temp directory
-with Favicons(base_favicon, generated_dir) as favicons:
+with Favicons(base_favicon, favicon_dir) as favicons:
     favicons.generate()
