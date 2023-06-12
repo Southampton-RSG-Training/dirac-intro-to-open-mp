47c1075fe7415829b090a9b9a5b7aaf8f6af3c08
Edward-RSE <94305757+Edward-RSE@users.noreply.github.com>
2023-06-12 13:44:12 +0000

    Deploying to gh-pages from @ Southampton-RSG-Training/dirac-intro-to-openmp@13fd880423acbf2dbc543bc1628e2c9374dfdd8d 🚀

--- bin/build_me.sh
+++ bin/build_me.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-
-echo 'Making favicons'
-python3 bin/make_favicons.py
-echo 'Making schedules'
+set -e -x
 python3 bin/get_schedules.py
-echo 'Making setup'
 python3 bin/get_setup.py
